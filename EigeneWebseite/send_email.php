8aed4e9c9e62441dc814e3108b73ef7496460f70
Atrox <hauschild-tim@t-online.de>
2022-01-10 13:01:53 +0100

    Minor changes (responsive)

--- EigeneWebseite/send_email.php
+++ EigeneWebseite/send_email.php
@@ -7,13 +7,16 @@ $name_von_emailfeld = "Email";   //Feld in der die Absenderadresse steht
 $empfaenger = "hauschild-tim@t-online.de"; //Empfänger-Adresse
 $mail_cc = ""; //CC-Adresse, diese E-Mail-Adresse bekommt einer weitere Kopie
 $betreff = "Betreff"; //Betreff der Email
+$placeholder = "";
+
+
 
 $url_ok = "index.html"; //Zielseite, wenn E-Mail erfolgreich versendet wurde
 $url_fehler = "Impressum.html"; //Zielseite, wenn E-Mail nicht gesendet werden konnte
 
 
 //Diese Felder werden nicht in der Mail stehen
-$ignore_fields = array();
+$ignore_fields = array('submit', 'Email', 'Betreff', 'Vorname', 'website');
 
 
 
@@ -28,13 +31,23 @@ $monat = date("m");
 $time = date("H:i");
 
 //Erste Zeile unserer Email
-$msg = "Gesendet am $tag, den $n.$monat.$jahr - $time Uhr\n\n";
+$msg = "Gesendet am $tag, den $n.$monat.$jahr - $time Uhr \n\n";
 
 //Hier werden alle Eingabefelder abgefragt
 foreach($_POST as $name => $value) {
+   if ($name == 'Vorname') {
+     $placeholder = $value;
+   }
+  
+   if ($name == 'Absender') {
+     $placeholder = $placeholder." $value";
+     $value = $placeholder;
+   }
+  
    if (in_array($name, $ignore_fields)) {
         continue; //Ignore Felder wird nicht in die Mail eingefügt
    }
+  
    $msg .= "$name: \n$value\n\n";
 }
 
@@ -57,8 +70,8 @@ if (!empty($mail_cc)) {
 //Email als UTF-8 senden
 $header .= "\nContent-type: text/plain; charset=utf-8";
 
-if(!empty($_POST['website'])) die();
 
+if(!empty($_POST['website'])) die();
 $mail_senden = mail($empfaenger,$betreff,$msg,$header);
 
 
